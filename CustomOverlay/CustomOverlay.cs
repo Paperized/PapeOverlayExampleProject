6ba65a0902787b59a21fc36a15dd58e1c0395f00
ivan lo greco <ivanlogreco99@outlook.it>
2021-03-15 18:44:44 +0100

    Example project

---
+++ CustomOverlay/CustomOverlay.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using GameOverlay.Drawing;
+using PapeOverlay;
+using PapeOverlay.Core;
+using PapeOverlay.Core.Layers;
+
+namespace CustomOverlay
+{
+    public class CustomOverlay : OverlayApplication
+    {
+        private GIFImageLayer duckGif;
+        private TextLayer textDuck;
+
+
+        public CustomOverlay(string path) : base(path)
+        {
+
+        }
+
+        public override AppConfig DefaultConfig()
+        {
+            // Default config for your overlay
+            return new AppConfig()
+            {
+                AuthorName = "MyAuthorName",
+                DescriptionOverlay = "My Description!",
+                Version = "1.0.0",
+                DestinationWindow = "Notepad"
+            };
+        }
+
+        protected override void InitializeComponents(WindowOverlay windowOverlay)
+        {
+            // Require anything you need here!
+            windowOverlay.SetBrush("red", Color.Red);
+            windowOverlay.SetBrush("white", new Color(255, 255, 255));
+
+            // Any assets goes is relative to the executable in the path ./Assets/xxxxxxx
+            windowOverlay.SetImage("duckAnimated", AssetsPath + "/duckGif.gif");
+            windowOverlay.SetFont("arial", "Arial", 16);
+        }
+
+        protected override void OnCreateLayers()
+        {
+            // Create every layer you need in here
+            Point windowSize = WindowOverlay.GetWindowSize(); 
+            Layer rootLayer = new Layer()
+            {
+                Name = "Root",
+            };
+            rootLayer.Transform.Position = windowSize.GetCenter();
+
+            duckGif = new GIFImageLayer()
+            {
+                Name = "Ducky Duck",
+            };
+            duckGif.SetImageName("duckAnimated");
+            duckGif.Transform.Parent = rootLayer.Transform;
+            duckGif.Transform.RelativePosition = new Point(-rootLayer.Transform.Position.X / 2, 0);
+
+            textDuck = new TextLayer()
+            {
+                Name = "Duck Text",
+                Text = "Ducks Are COOOOL!",
+                FontSize = 24
+            };
+            textDuck.SetBrushColorName("red");
+            textDuck.SetFontName("arial");
+            textDuck.Transform.Parent = rootLayer.Transform;
+            textDuck.Transform.RelativePosition = new Point(rootLayer.Transform.Position.X / 2, 0);
+
+
+            WindowOverlay.RootLayer = rootLayer;
+        }
+
+        protected override void OnUpdateOverlay(float deltaTime, long frameCount, float frameTime)
+        {
+            
+        }
+    }
+}
---
+++ CustomOverlay/Program.cs
@@ -0,0 +1,94 @@
+﻿using PapeOverlay;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomOverlay
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyCustomApplicationContext());
+        }
+
+        public class MyCustomApplicationContext : ApplicationContext
+        {
+            private readonly NotifyIcon trayIcon;
+            private OverlayApplication overlayApplication;
+
+            public MyCustomApplicationContext()
+            {
+                // Initialize Tray Icon
+                trayIcon = new NotifyIcon()
+                {
+                    Icon = Icon.ExtractAssociatedIcon("./icon.png"),
+                    Visible = true
+                };
+
+                trayIcon.ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Start Overlay", Start),
+                    new MenuItem("Quit", Close)
+                });
+            }
+
+            private void Start(object sender, EventArgs e)
+            {
+                if (overlayApplication == null || !overlayApplication.IsStarted)
+                {
+                    overlayApplication = new CustomOverlay(System.AppDomain.CurrentDomain.BaseDirectory + "/overlay");
+                    overlayApplication.OnEvent += OverlayApplication_OnEvent;
+                    overlayApplication.OnError += OverlayApplication_OnError;
+                    overlayApplication.InitializeOverlay();
+                    overlayApplication.Start();
+                }
+            }
+
+            private void OverlayApplication_OnError(OverlayApplication arg1, ApplicationError arg2)
+            {
+                trayIcon.ShowBalloonTip(2000, arg2.title, arg2.message, GetErrorToolTipIcon(arg2.severity));
+                if(arg2.severity == ApplicationErrorSeverity.Fatal)
+                {
+                    Thread.Sleep(4000);
+                    Close(null, null);
+                }
+            }
+
+            private ToolTipIcon GetErrorToolTipIcon(ApplicationErrorSeverity severity)
+            {
+                if (severity == ApplicationErrorSeverity.Fatal)
+                    return ToolTipIcon.Error;
+                else
+                    return ToolTipIcon.Warning;
+            }
+
+            private void OverlayApplication_OnEvent(OverlayApplication arg1, ApplicationEvent arg2)
+            {
+                trayIcon.ShowBalloonTip(2000, arg2.title, arg2.message, ToolTipIcon.Info);
+            }
+
+            private void Close(object sender, EventArgs e)
+            {
+                // Hide tray icon, otherwise it will remain shown until user mouses over it
+                trayIcon.Visible = false;
+                if (overlayApplication != null && overlayApplication.IsStarted)
+                {
+                    overlayApplication.OnEvent -= OverlayApplication_OnEvent;
+                    overlayApplication.OnError -= OverlayApplication_OnError;
+                    overlayApplication.Close();
+                }
+                Application.Exit();
+            }
+        }
+    }
+}
